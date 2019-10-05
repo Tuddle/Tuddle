2c018bef0e4be19a6aba59e1e6ec416ae031e8df
Zadique Carey <vendettastrike@gmail.com>
2019-10-04 19:45:21 -0500

    "first commit"

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener("click", ()=>{
     navLinks.classList.toggle("open");
 });
+
+function homePage(){
+    window.location.href = 'https://tuddle.github.io/tuddle/';
+}
